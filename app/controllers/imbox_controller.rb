caac260027eb874c14e1a55c9385d7ee82a2d4ca
Jake Zien <jake@floored.com>
2021-06-28 17:19:59 -0400

    imbox trays

--- app/controllers/imbox_controller.rb
+++ app/controllers/imbox_controller.rb
@@ -7,6 +7,13 @@ class ImboxController < ApplicationController
 		}.sort{
 			|a, b| b.messages.last[:time_received] <=> a.messages.last[:time_received]
 		}
+
+		# Yeah, this is inefficient, but I'm running out of time :/
+		@set_aside = Topic.all.filter {
+			|topic| topic.set_aside?
+		}.first(7).sort{
+			|a, b| b.messages.last[:time_received] <=> a.messages.last[:time_received]
+		}
 	end
 
 end
